c28455fef00da2f5021fd1f4e0a601bed9ed2b00
lbfrias <levinfrias@gmail.com>
2025-07-10 07:11:24 +0800

    Test selector translation

--- custom_components/gree/config_flow.py
+++ custom_components/gree/config_flow.py
@@ -268,6 +268,7 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
                             options=DEFAULT_FAN_MODES,
                             multiple=True,
                             custom_value=True,
+                            translation_key="option"
                         )
                     )
                 ),
